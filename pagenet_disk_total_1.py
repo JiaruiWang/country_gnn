d4e417077fcccb2867e8be3c72cd6b07d7f7445f
JiaruiWang <jiarui.wang523@gmail.com>
2022-04-26 11:54:10 -0700

    comment lines in disk total

--- pagenet_disk_total_1.py
+++ pagenet_disk_total_1.py
@@ -137,27 +137,27 @@ class PagenetDataset(Dataset):
 
 
 # %% test the dataset 
-root = "data/"
-dataset = PagenetDataset(root)
-data = dataset.get()
+# root = "data/"
+# dataset = PagenetDataset(root)
+# data = dataset.get()
 
 # %% examine the graph
-print(f'Dataset: {data}:')
-print('======================')
-print(f'Number of graphs: {len(data)}')
-print(data)
-print('===========================================================================================================')
+# print(f'Dataset: {data}:')
+# print('======================')
+# print(f'Number of graphs: {len(data)}')
+# print(data)
+# print('===========================================================================================================')
 
 # Gather some statistics about the graph.
-print(f'Number of nodes: {data.num_nodes}')
-print(f'Number of features: {data.num_features}')
-print(f'Number of classes: {data.num_classes}')
-print(f'Number of edges: {data.num_edges}')
-print(f'Average node degree: {data.num_edges / data.num_nodes:.2f}')
-print(f'Number of training nodes: {data.train_mask.sum()}')
-print(f'Number of validation nodes: {data.val_mask.sum()}')
-print(f'Number of testing nodes: {data.test_mask.sum()}')
-print(f'Training node label rate: {int(data.train_mask.sum()) / data.num_nodes:.2f}')
+# print(f'Number of nodes: {data.num_nodes}')
+# print(f'Number of features: {data.num_features}')
+# print(f'Number of classes: {data.num_classes}')
+# print(f'Number of edges: {data.num_edges}')
+# print(f'Average node degree: {data.num_edges / data.num_nodes:.2f}')
+# print(f'Number of training nodes: {data.train_mask.sum()}')
+# print(f'Number of validation nodes: {data.val_mask.sum()}')
+# print(f'Number of testing nodes: {data.test_mask.sum()}')
+# print(f'Training node label rate: {int(data.train_mask.sum()) / data.num_nodes:.2f}')
 # print(f'Has isolated nodes: {data.has_isolated_nodes()}')
 # print(f'Has self-loops: {data.has_self_loops()}')
 # print(f'Is undirected: {data.is_undirected()}')
